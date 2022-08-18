67b06d1382eb3d568e29ead13ce0db09dd6d9500
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-08-18 08:14:44 +0530

    DB connected

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from "cors"
+import restaurants from "./api/restaurants.route.js"
+
+// creating express app
+const app = express()
+
+// apply middleware
+app.use(cors())
+app.use(express.json())
+
+// routes
+app.use("api/v1/restaurants", restaurants)
+app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
+
+export default app;
